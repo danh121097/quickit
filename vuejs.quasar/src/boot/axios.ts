a2edcbdb62f37df07ee8365f304678a35baf6019
danhnguyen <danhnguyen@beeknights.com>
2025-05-13 23:39:15 +0700

    refactor:  code structure for improved readability and maintainability

--- vuejs.quasar/src/boot/axios.ts
+++ vuejs.quasar/src/boot/axios.ts
@@ -1,37 +1,33 @@
-import axios, { AxiosError } from 'axios';
-import { AxiosInstance, AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios';
+import axios, { AxiosError, AxiosRequestHeaders } from 'axios';
+import { InternalAxiosRequestConfig } from 'axios';
 import { IAPIResponseError } from '@types';
 import Base64 from 'crypto-js/enc-base64';
 import HmacSHA256 from 'crypto-js/hmac-sha256';
 
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-  }
-}
-
-enum ContentType {
-  'application/json' = 'application/json',
-}
-
-interface AxiosConfigHeaders {
-  'Content-Type': ContentType;
-  ctime?: number;
-  sig?: string;
-  authorization?: string;
-}
-
+// API_END_POINT: 'https://api.example.com';
+// API_PREFIX: 'v1';
 const api = axios.create({
   baseURL: `${process.env.API_END_POINT}/${process.env.API_PREFIX}`,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: '*/*',
+  },
   withCredentials: true,
 });
 
-api.interceptors.request.use(
-  function (config: AxiosRequestConfig) {
-    setHeaders(config);
-    deleteEmptyValues(config);
+function setHeaders(config: InternalAxiosRequestConfig): AxiosRequestHeaders {
+  return Object.assign({}, generateHMACSignature(config), config.headers);
+}
 
-    return config as InternalAxiosRequestConfig<any> | Promise<InternalAxiosRequestConfig<any>>;
+api.interceptors.request.use(
+  function (config) {
+    config.headers = setHeaders(config);
+
+    const token = LocalStorage.getItem(`${process.env.APP_NAME}_TOKEN`) as string | undefined;
+
+    if (token) config.headers.authorization = `Bearer ${token}`;
+
+    return config;
   },
   function (error) {
     return Promise.reject(error instanceof Error ? error : new Error(String(error)));
@@ -53,48 +49,19 @@ api.interceptors.response.use(
   },
 );
 
-function deleteEmptyValues(config: AxiosRequestConfig) {
-  if (config.data) config.data = deleteEmptyValue(config.data as Record<string, unknown>);
-
-  if (config.params) config.params = deleteEmptyValue(config.params as Record<string, unknown>);
-}
-
-function deleteEmptyValue(data: Record<string, unknown>) {
-  Object.keys(data).map((k) => {
-    data[k] == void 0 && delete data[k];
-  });
-  return data;
-}
-
-function setHeaders(config: AxiosRequestConfig) {
-  const token = LocalStorage.getItem(`${process.env.APP_NAME}_TOKEN`);
-
-  config.headers = {
-    ...generateHMACSignature(config),
-    ...config.headers,
-  };
-  if (token && typeof token === 'string')
-    (config.headers as AxiosConfigHeaders).authorization = `Bearer ${token}`;
-}
-
-function generateHMACSignature(config: AxiosRequestConfig) {
-  const headers: AxiosConfigHeaders = {
-    'Content-Type': ContentType['application/json'],
-    ctime: +new Date(),
-  };
-
+function generateHMACSignature(config: InternalAxiosRequestConfig<any>) {
   let path = config.url || '';
   if (!path.startsWith('/')) path = '/' + path;
 
   const method = config.method?.toUpperCase() || '';
-  const contentType = headers['Content-Type'];
-  const ctime = headers['ctime'] || '';
+  const contentType = config.headers['Content-Type'] as string;
+  const ctime = +new Date();
 
-  const stringToSign = `${method}\n${contentType}\n${ctime}\n${path}\n`;
+  const stringToSign = [method, contentType, ctime, path].join('\n');
 
-  headers.sig = Base64.stringify(HmacSHA256(stringToSign, process.env.HMAC_SECRET || ''));
+  const sig = Base64.stringify(HmacSHA256(stringToSign, process.env.HMAC_SECRET || ''));
 
-  return headers;
+  return { sig, ctime };
 }
 
 export { api };
